ea6b2056e84f6bd75fedc293a64d233a1366ec5f
mennareda84 <mennareda8484@gmail.com>
2025-08-24 17:39:07 +0300

    Fix code and update waveform results

--- X_plus_Ysquared/src/XY2_adder.vhd
+++ X_plus_Ysquared/src/XY2_adder.vhd
@@ -1,13 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity XY2_Adder is
+entity XY2_adder is
 port (Y: in std_logic_vector(1 downto 0);
       X: in std_logic_vector(1 downto 0);
       Z: out std_logic_vector(3 downto 0));
-end XY2_Adder;
+end XY2_adder;
 
-architecture Structural of XY2_Adder is
+architecture Structural of XY2_adder is
 component mux4_1 is
 Port ( A,B,C,D: in std_logic;
       Sel: in std_logic_vector (1 downto 0);
@@ -37,7 +37,7 @@ decoder_input <= Y & X;
 
 -- The equation: Z = X + (Y)**2
 decoder: decoder4_16 port map (en, decoder_input, decoder_output);
-Z(0) <= decoder_output(1) or decoder_output(3) or decoder_output(4) or decoder_output(9) or decoder_output(11) or decoder_output(12) or decoder_output(14);
+Z(0) <= decoder_output(1) or decoder_output(3) or decoder_output(4) or decoder_output(6) or decoder_output(9) or decoder_output(11) or decoder_output(12) or decoder_output(14);
 mux1: mux4_1 port map (X(1), x1_xor_x0, X(1), x1_xor_x0, selector, Z(1));
 mux2: mux4_1 port map ('0', x1_and_x0, '1', x1_and_x0, selector, Z(2));
 Z(3) <= Y(1) and Y(0);
